5a728d77d82547e1bdc1369f52c8d848c359603f
Sylvain Lorinet <drackster@gmail.com>
2013-08-26 21:01:13 +0200

    Add timezone parameter

--- vagrant_bootstrap/parameters.sh
+++ vagrant_bootstrap/parameters.sh
@@ -1,4 +1,3 @@
-
 # PARAMETERS
 # ----------
 
@@ -8,3 +7,7 @@ DATABASE_NAME="your_database_name"
 DATABASE_USER="your_user"
 DATABASE_PASSWORD="your_user_password"
 DATABASE_HOST="%" # Replace "%" by "localhost" for local user only, 10.0.2.2 for host only or any IP address
+
+# PHP
+# ---
+PHP_TIMEZONE="UTC"
