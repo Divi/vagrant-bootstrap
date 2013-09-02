7346ccbe1b0b94dd7069eceeb63568c81da9bda4
Divi <sylvain.lorinet@gmail.com>
2013-09-02 17:13:33 +0200

    Update from master

--- .vagrant_bootstrap/custom.sh
+++ .vagrant_bootstrap/custom.sh
@@ -7,18 +7,10 @@
 # CUSTOM BOOSTRAP FOR SYMFONY2
 # ----------------------------
 
-# Delete symlink /var/www and create folder
-rm -rf /var/www
-ln -fs /home/project/web /var/www
-
 # VHOST
 cp /etc/apache2/sites-available/symfony2.conf.dist /etc/apache2/sites-available/000-default.conf
 # Reload apache
 service apache2 reload
-
-# Run composer install
-cd /home/project
-php composer.phar install
 
 # Apply chmod for cache & logs
 mkdir /dev/shm/$APPLICATION_NAME
