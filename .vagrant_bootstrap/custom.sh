1e154bed8c3a3dc83ec79bc5e22b9311c4c4a3d3
Divi <sylvain.lorinet@gmail.com>
2013-09-02 16:57:57 +0200

    Some depreacated updates

--- .vagrant_bootstrap/custom.sh
+++ .vagrant_bootstrap/custom.sh
@@ -1,54 +1,25 @@
+#!/bin/bash
+
+# ----------------------------------------
+# https://github.com/Divi/VagrantBootstrap
+# ----------------------------------------
+
 # CUSTOM BOOSTRAP FOR SYMFONY2
 # ----------------------------
 
-# Delete symlink /var/www and create new folder
+# Delete symlink /var/www and create folder
 rm -rf /var/www
-mkdir /var/www
+ln -fs /home/project/web /var/www
 
 # VHOST
-VHOST=$(cat <<EOF
-<VirtualHost *:80>
-	DocumentRoot "/var/www/web"
-	DirectoryIndex app_dev.php
-	ServerName localhost
-	
-	<Directory "/var/www/web">
-		AllowOverride None
-		Allow from All
-		<IfModule mod_rewrite.c>
-			RewriteEngine On
-			
-			RewriteCond %{REQUEST_URI}::$1 ^(/.+)/(.*)::\2$
-			RewriteRule ^(.*) - [E=BASE:%1]
-			
-			RewriteCond %{ENV:REDIRECT_STATUS} ^$
-			RewriteRule ^app_dev\.php(/(.*)|$) %{ENV:BASE}/$2 [R=301,L]
-			
-			RewriteCond %{REQUEST_FILENAME} -f
-			RewriteRule .? - [L]
-			
-			RewriteRule .? %{ENV:BASE}/app_dev.php [L]
-		</IfModule>
-	</Directory>
-	
-	ErrorLog "/var/www/app/logs/apache_app_errors.log"
-</VirtualHost>
-EOF
-)
-echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+cp /etc/apache2/sites-available/symfony2.conf.dist /etc/apache2/sites-available/000-default.conf
 # Reload apache
 service apache2 reload
-# Clear www folder
-rm -rf /var/www/index.html
-
-# Add required files
-cp /vagrant/composer.json /var/www
-cp /vagrant/composer.lock /var/www
-cp /vagrant/composer.phar /var/www
 
 # Run composer install
-cd /var/www
+cd /home/project
 php composer.phar install
 
 # Apply chmod for cache & logs
+mkdir /dev/shm/$APPLICATION_NAME
 chmod 777 -R /dev/shm/$APPLICATION_NAME
